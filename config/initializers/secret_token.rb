7cb64f9d4e55ddfe818285a16201c84770abdb9e
James C. Roberts <jroberts@market76.com>
2014-02-20 04:02:44 -0500

    Initial commit of all site files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+X::Application.config.secret_token = 'c613218de43360996ea7c74f77ea327d565010ffbf53bd2ff766f70a1d4c2560c086ca3a29938cc7374cfba3c8708a77c58fe0a08cc30b3f5f5827dce06fb6ff'
