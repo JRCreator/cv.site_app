6a89136cd5d333ba2f4a941f11e3928437007f91
James C. Roberts <jroberts@market76.com>
2014-02-20 04:41:46 -0500

    routes.rb update

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ X::Application.routes.draw do
   resources :articles
 
 
-root :to => "articles#index"
+  root :to => "articles#index"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
